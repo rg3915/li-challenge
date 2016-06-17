6d73808210433144b9831a75831bfe2d795894bf
rg3915 <rg3915@yahoo.com.br>
2016-06-17 11:02:31 -0300

    test_colorize_vertical_interval

--- editor_grafico.py
+++ editor_grafico.py
@@ -46,6 +46,20 @@ class TestUnit(unittest.TestCase):
         ]
         self.assertEqual(expected, self.matrix.as_list)
 
+    def test_colorize_vertical_interval(self):
+        """
+        V X Y1 Y2 C
+        Desenha um segmento vertical na coluna X nas linhas de Y1 a Y2
+        (intervalo inclusivo) na cor C.
+        """
+        self.matrix.create(2, 3)
+        self.matrix.colorize_vertical_interval(2, 1, 2, 'C')
+        expected = [
+            ['O', 'C', 'O'],
+            ['O', 'C', 'O'],
+        ]
+        self.assertEqual(expected, self.matrix.as_list)
+
 
 class Matrix(object):
 
@@ -67,6 +81,11 @@ class Matrix(object):
     def colorize_pixel(self, row, col, color):
         col, row = col - 1, row - 1
         self.as_list[row][col] = color
+
+    def colorize_vertical_interval(self, col, line_start, line_end, color):
+        for line in range(line_start, line_end + 1):
+            index = line - 1
+            self.as_list[index][col - 1] = color
 
 
 def main():
